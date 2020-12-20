82bb87c076a7a205aa54caf83495c6c132d087f1
Zaid Mughal <51900989+zaidMughal@users.noreply.github.com>
2020-12-20 18:58:21 +0500

    Winner function and minor changes

--- contracts/SecVote.sol
+++ contracts/SecVote.sol
@@ -20,9 +20,9 @@ contract SecureVote {
 
     struct Candidate {
         // If you can limit the length to a certain number of bytes, 
-        // always use one of bytssses1 to bytes32 because they are much cheaper
+        // always use one of bytes1 to bytes32 because they are much cheaper. But I guess you can't use bytes32 as fixed strings
         //govtID id;
-        bytes32 name;
+        string name;    //bytes32 name;
         uint16 party; //hold party as code, store codes externally
         uint16 constituency;
         //string logo; //logo should be accessed externally
@@ -63,7 +63,7 @@ contract SecureVote {
         isPollingAgent[_paAddr] = true;
     }
 
-    function registerCandidate(uint64 _cnic, bytes32 _name, uint16 _party, uint16 _const) public onlyOwner {
+    function registerCandidate(uint64 _cnic, string memory _name, uint16 _party, uint16 _const) public onlyOwner {
         //TODO: decide if we should bind candidates / voters with blockchain addresses.
         constituencyCandidates[_const].push(_cnic);
         candidateDetails[_cnic].name = _name;
@@ -76,12 +76,13 @@ contract SecureVote {
         return constituencyCandidates[_const];
     }
 
-    function getCandidateName(uint64 _cnic) public view returns (bytes32) { //can be handled externally.
+    function getCandidateName(uint64 _cnic) public view returns (string memory) { //can be handled externally.
         return candidateDetails[_cnic].name;
     }
 
     function registerVoter(uint64 _cnic) public  onlyOwner {//Owner will register. Polling agents will only cross check identity and let him vote.
         voterDetails[_cnic].registered = true;
+        voterDetails[_cnic].constituency = _const;
     }
     
     function vote(uint64 _cnic, uint64 voted_cnic) public voterRegistered(_cnic) {
@@ -90,6 +91,19 @@ contract SecureVote {
         voterDetails[_cnic].voted_time = block.timestamp;       //But some calculation will be needed since it returns current block timestamp as seconds since unix epoch
         candidateDetails[voted_cnic].voteCount++;
     }
+    
+    function winner(uint16 _const) public view returns(uint64 cnic_){
+        uint64 max = 0;
+        uint64 winner_ = 0;
+        uint64[] memory candi = getConstituencyCandidates(_const);
+        for(uint i=0;i<candi.length;i++){
+            if(candidateDetails[candi[i]].voteCount >max){
+                max = candidateDetails[candi[i]].voteCount;
+                winner_ = candi[i];
+            }
+        }
+        cnic_ = winner_; 
+    }
 
     //https://github.com/chrisdotn/jsmnSol/blob/master/contracts/JsmnSolLib.sol
     // function registerCandidateFromSheet() public onlyOwner {  //Can't figure out how to import json
@@ -97,3 +111,10 @@ contract SecureVote {
     // }
     
 }
+/*
+Possible voting method:
+Voter will get his constituency number from NADRA(Every constituency is mapped to a number). Every voter can vote only to candidates of his/her constituency.
+In polling boaths, polling agents(one from every party) will verify if voter is registered or not and if he has already voted or not.
+After verifying, polling agents will let him/her vote from a Dapp in a node/laptop.
+He will fill his constituency, and cnic(only for voting record and will not be recorded against a vote). Then select symbol of selected candidate and vote complete.
+
